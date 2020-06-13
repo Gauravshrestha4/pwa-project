9bed2b450572d7fc3d600f70fe0e3162617cb30c
Gaurav Shrestha <gaurav.shrestha@shiksha.com>
2020-06-14 02:26:36 +0530

    added basic caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,14 +1,22 @@
+
 var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.
-        register('/sw.js').then(function () {
-            console.log('service worker registered');
-        })
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('befire install evnet fired')
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
-})
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+ 
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,58 @@
-self.addEventListener('install',e=>console.log('[SW] installing SW',e))
-self.addEventListener('activate', e => {
-    console.log('[SW] activating SW', e);
-    return self.clients.claim();
-})
+var CACHE_STATIC_NAME = 'static-v3'
+var CACHE_DYNAMIC_NAME='dynamic'
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(caches.open('static-v2')
+    .then(function (cache) {
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
+     
+  }))
+});
 
-self.addEventListener('fetch', (e) => {
-    console.log('fetching data',e);
-    return e.respondWith(fetch(e.request));
-})
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys().
+      then(function (keyList) {
+        return Promise.all(keyList.map(key => {
+          console.log('deleting caches');
+          if (key != CACHE_STATIC_NAME && key != CACHE_DYNAMIC_NAME) return caches.delete(key);
+      }))
+    })
+  )
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+        return response
+        }
+        else {
+          return fetch(event.request).
+            then(res => {
+              return caches.open('dynamic').
+                then(cache => {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+              })
+          })
+        }
+    })
+  );
+});
